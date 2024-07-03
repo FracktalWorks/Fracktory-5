1949569911865428654c160fcb3b496a6d563d83
Vijay Raghav Varada <vjvarada@users.noreply.github.com>
2024-07-03 21:16:55 +0530

    added 0.6mm intent

--- fileRenamer.py
+++ fileRenamer.py
@@ -2,15 +2,16 @@ import os
 
 if __name__ == "__main__":
 
-    folderName = input("Enter Path of Folder to change names within: ")
+    #folderName = input("Enter Path of Folder to change names within: ")
     oldChar = input("Enter Characters to replace in File Names: ")
     newChar = input("Enter Characters to replace with in File Names: ")
     filesPaths = []
+    folderNames = [r'C:\Users\Vijay\Documents\GitHub\Fracktory-5\resources\intent\base_fracktal_printer\ABS\Model 0.6',r'C:\Users\Vijay\Documents\GitHub\Fracktory-5\resources\intent\base_fracktal_printer\CF-Nylon\Model 0.6',r'C:\Users\Vijay\Documents\GitHub\Fracktory-5\resources\intent\base_fracktal_printer\Nylon\Model 0.6',r'C:\Users\Vijay\Documents\GitHub\Fracktory-5\resources\intent\base_fracktal_printer\PETG\Model 0.6',r'C:\Users\Vijay\Documents\GitHub\Fracktory-5\resources\intent\base_fracktal_printer\PLA\Model 0.6']
 
-
-    for (root,dirs,files) in os.walk(folderName):
-        for file in files:
-            filesPaths.append(os.path.join(root,file))
+    for folderName in folderNames:
+         for (root,dirs,files) in os.walk(folderName):
+            for file in files:
+                filesPaths.append(os.path.join(root,file))
     print('Files Changed: \n')
     for path in filesPaths:
         newname = path.replace(oldChar, newChar)
